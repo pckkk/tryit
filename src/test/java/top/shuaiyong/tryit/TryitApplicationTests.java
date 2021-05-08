e11cca2a919fc0cd78ba8ec0c98ee5eb2622b878
liushuaiyong <17600763540@163.com>
2021-05-08 10:45:39 +0800

    新建项目

---
+++ src/test/java/top/shuaiyong/tryit/TryitApplicationTests.java
@@ -0,0 +1,13 @@
+package top.shuaiyong.tryit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
